5bc974f1990c4aef5d9d9a78da89c1776f55e120
hwangjunhong <ghkdwnsghd1@naver.com>
2018-07-14 11:40:58 +0900

    reservation

---
+++ app/src/main/java/com/example/studyroom/Model/ReserveCompanyModel.java
@@ -0,0 +1,16 @@
+package com.example.studyroom.Model;
+
+/**
+ * Created by hong on 2018-05-11.
+ */
+
+public class ReserveCompanyModel {
+
+    String phoneNumber;
+    String userName;
+    String roomType;
+    String roomInfo;
+    String startTime;
+    String endTime;
+
+}
