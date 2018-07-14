7a7ff74f71a50dc113fba6302a107c04ddbc3cec
hwangjunhong <ghkdwnsghd1@naver.com>
2018-07-14 13:01:16 +0900

    12

--- app/src/main/java/com/example/studyroom/TestClass.java
+++ app/src/main/java/com/example/studyroom/TestClass.java
@@ -1,4 +1,15 @@
 package com.example.studyroom;
 
-public class TestClass {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TestClass extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
